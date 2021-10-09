ca3444b612460080d39966494118d5c0726be110
Lucy Woodman <lucyjwoodman@gmail.com>
2021-10-09 08:09:16 +0100

    refac: move game setup code to separate files

---
+++ assets/js/fetch.js
@@ -0,0 +1,25 @@
+async function getHints() {
+  fetch(`https://dictionaryapi.com/api/v3/references/learners/json/${correctAnswer}?key=df6eee5d-53f0-48ce-a696-76eabea17b10`)
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => createHint(data[0].shortdef[0]))
+    .catch(err => {console.error(err);});
+  
+  // HELPER FUNCTIONS
+  function checkStatus(response) {
+    if(response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
+  
+  function createHint(data) {
+    let hints = `
+        <p>${data}</p>
+    `;
+  
+    console.log(hints);
+    document.getElementById('answer-hints').innerHTML = hints;
+  }
+}
---
+++ assets/js/game.js
@@ -0,0 +1,147 @@
+let answerWords = [
+    "accident",
+    "actual",
+    "address",
+    "answer",
+    "appear",
+    "arrive",
+    "believe",
+    "bicycle",
+    "breath",
+    "breathe",
+    "build",
+    "busy",
+    "calendar",
+    "caught",
+    "centre",
+    "century",
+    "certain",
+    "circle",
+    "complete",
+    "consider",
+    "continue",
+    "decide",
+    "describe",
+    "different",
+    "difficult",
+    "disappear",
+    "early",
+    "earth",
+    "eight",
+    "enough",
+    "exercise",
+    "experience",
+    "experiment",
+    "extreme",
+    "famous",
+    "favourite",
+    "February",
+    "forward",
+    "fruit",
+    "grammar",
+    "group",
+    "guard",
+    "guide",
+    "heard",
+    "heart",
+    "height",
+    "history",
+    "imagine",
+    "increase",
+    "important",
+    "interest",
+    "island",
+    "knowledge",
+    "learn",
+    "length",
+    "library",
+    "material",
+    "medicine",
+    "mention",
+    "minute",
+    "natural",
+    "naughty",
+    "notice",
+    "occasion",
+    "often",
+    "opposite",
+    "ordinary",
+    "particular",
+    "peculiar",
+    "perhaps",
+    "popular",
+    "position",
+    "possess",
+    "possible",
+    "potatoes",
+    "pressure",
+    "probably",
+    "promise",
+    "purpose",
+    "quarter",
+    "question",
+    "recent",
+    "regular",
+    "reign",
+    "remember",
+    "sentence",
+    "separate",
+    "special",
+    "straight",
+    "strange",
+    "strength",
+    "suppose",
+    "surprise",
+    "therefore",
+    "though",
+    "thought",
+    "through",
+    "various",
+    "weight",
+    "woman"
+];
+
+let correctAnswer = '';
+let donutLeft = 100;
+let guessedLetters = [];
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let guessState = null;
+
+function randomWord() {
+    correctAnswer = answerWords[Math.floor(Math.random() * answerWords.length)];
+    console.log(correctAnswer);
+}
+
+function displayHiddenWord() {
+    let hiddenWord = '';
+    for (let {} of correctAnswer) {
+        hiddenWord += '_';
+    }
+    document.getElementById('game-word').innerHTML = hiddenWord;
+}
+
+function createButtons() {
+    let html = '';
+    for (const letter of alphabet) {
+        html += `<button class="key" id="${letter}">${letter}</button>`;
+    }
+    document.getElementById('keyboard').innerHTML = html;
+}
+
+function addKeyListeners() {
+    document.addEventListener('keydown', function (event) {
+        let key = event.key.toLowerCase();
+        // If the pressed key is not included in the alphabet, log a message to the console
+        alphabet.includes(key) ? checkAnswer(key) : console.log("You must select a letter");
+    });
+    
+    let keyboardKeys = document.getElementsByTagName('button');
+    
+    for (const keyboardKey of keyboardKeys) {
+        keyboardKey.addEventListener('click', function () {
+            let keyboardKey = this.id.toLowerCase();
+        // If the selected key is not included in the alphabet, log a message to the console
+        alphabet.includes(keyboardKey) ? checkAnswer(keyboardKey) : console.log("You must select a letter");
+    })}
+}
+
