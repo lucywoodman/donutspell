7d2d3dbb68181d22579317548655578ad848ca26
Lucy Woodman <lucyjwoodman@gmail.com>
2021-10-12 06:46:04 +0100

    refac: add comments to walk through code

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,10 @@
 (function() {
+    // Waits for the DOM to finish loading before adding submit button event listeners.
     document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('contact-form').addEventListener('submit', function (event) {
+            // Prevents the default submit action.
             event.preventDefault();
+            // Uses the EmailJS API to send the form input.
             emailjs.init("user_e0qnu0neIitMWOhxmH7Yu");
             emailjs.sendForm('service_x1hrmtx', 'template_lgttxpk', this)
                 .then(function () {
@@ -10,11 +13,14 @@
                     // error message
                     console.log('FAILED...', error);
                 });
+            // Displays a thank you message.
             displayThanks();
         });
     });
     
-    
+    /**
+     * Replaces the form HTML with a thank you message after the form has been submitted.
+     */
     function displayThanks() {
         let html = `
                 <h2>Success!</h2>
