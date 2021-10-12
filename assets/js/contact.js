87bf0a0b5dcfd78d3f55690082ca5623012f6e1a
Lucy Woodman <lucyjwoodman@gmail.com>
2021-10-12 06:21:09 +0100

    refac: wrap code in a function to reduce possibility of conflict

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        emailjs.init("user_e0qnu0neIitMWOhxmH7Yu");
-        emailjs.sendForm('service_x1hrmtx', 'template_lgttxpk', this)
-            .then(function () {
-                // success sending email
-            }, function (error) {
-                // error message
-                console.log('FAILED...', error);
-            });
-        displayThanks();
+(function() {
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById('contact-form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            emailjs.init("user_e0qnu0neIitMWOhxmH7Yu");
+            emailjs.sendForm('service_x1hrmtx', 'template_lgttxpk', this)
+                .then(function () {
+                    // success sending email
+                }, function (error) {
+                    // error message
+                    console.log('FAILED...', error);
+                });
+            displayThanks();
+        });
     });
-});
-
-
-function displayThanks() {
-    let html = `
-            <h2>Success!</h2>
-            <p>Your message has been sent. Thank you for getting in touch.</p>
-            <a href="index.html" class="button">Return home</a>
-            `;
-    document.getElementsByClassName('form')[0].innerHTML = html;
-}
+    
+    
+    function displayThanks() {
+        let html = `
+                <h2>Success!</h2>
+                <p>Your message has been sent. Thank you for getting in touch.</p>
+                <a href="index.html" class="button">Return home</a>
+                `;
+        document.getElementsByClassName('form')[0].innerHTML = html;
+    }
+})();
