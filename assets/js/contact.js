b0d2b48f6d29d015486aed104fa542cf9749e298
Lucy Woodman <lucyjwoodman@gmail.com>
2021-10-13 17:33:42 +0100

    feat: add JS form validation

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,104 @@
 (function() {
-    // Waits for the DOM to finish loading before adding submit button event listeners.
-    document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('contact-form').addEventListener('submit', function (event) {
-            // Prevents the default submit action.
-            event.preventDefault();
+    const nameField = document.getElementById('name');
+    const emailField = document.getElementById('email');
+    const messageField = document.getElementById('message');
+
+    const form = document.getElementById('contact-form');
+
+    const checkName = () => {
+        let valid = false;
+        const min = 3,
+            max = 25;
+        const name = nameField.value.trim();
+        if (!isRequired(name)) {
+            showError(nameField, 'Name cannot be blank.');
+        } else if (!isBetween(name.length, min, max)) {
+            showError(nameField, `Name must be between ${min} and ${max} characters.`)
+        } else {
+            showSuccess(nameField);
+            valid = true;
+        }
+        return valid;
+    };
+
+    const checkEmail = () => {
+        let valid = false;
+        const email = emailField.value.trim();
+        if (!isRequired(email)) {
+            showError(emailField, 'Email cannot be blank.');
+        } else if (!isEmailValid(email)) {
+            showError(emailField, 'Email is not valid.')
+        } else {
+            showSuccess(emailField);
+            valid = true;
+        }
+        return valid;
+    };
+
+    const checkMessage = () => {
+        let valid = false;
+        const min = 30;
+        const message = messageField.value.trim();
+        if (!isRequired(message)) {
+            showError(messageField, 'Username cannot be blank.');
+        } else if (!isBetween(message.length, min)) {
+            showError(messageField, `Message must be at least ${min} characters.`)
+        } else {
+            showSuccess(messageField);
+            valid = true;
+        }
+        return valid;
+    };
+
+    const isEmailValid = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
+    const isRequired = value => value === '' ? false : true;
+    const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+    const showError = (input, message) => {
+        // get the form-field element
+        const formField = input.parentElement;
+        // add the error class
+        formField.classList.remove('success');
+        formField.classList.add('error');
+    
+        // show the error message
+        const error = formField.querySelector('small');
+        error.textContent = message;
+    };
+    
+    const showSuccess = (input) => {
+        // get the form-field element
+        const formField = input.parentElement;
+    
+        // remove the error class
+        formField.classList.remove('error');
+        formField.classList.add('success');
+    
+        // hide the error message
+        const error = formField.querySelector('small');
+        error.textContent = '';
+    }
+    
+    
+    form.addEventListener('submit', function (e) {
+        // prevent the form from submitting
+        e.preventDefault();
+    
+        // validate fields
+        let isNameValid = checkName(),
+            isEmailValid = checkEmail(),
+            isMessageValid = checkMessage();
+    
+        let isFormValid = isNameValid &&
+            isEmailValid &&
+            isMessageValid;
+    
+        // submit to the server if the form is valid
+        if (isFormValid) {
             // Uses the EmailJS API to send the form input.
             emailjs.init("user_e0qnu0neIitMWOhxmH7Yu");
             emailjs.sendForm('service_x1hrmtx', 'template_lgttxpk', this)
@@ -15,8 +110,37 @@
                 });
             // Displays a thank you message.
             displayThanks();
-        });
+        }
     });
+    
+    
+    const debounce = (fn, delay = 500) => {
+        let timeoutId;
+        return (...args) => {
+            // cancel the previous timer
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            // setup a new timer
+            timeoutId = setTimeout(() => {
+                fn.apply(null, args)
+            }, delay);
+        };
+    };
+    
+    form.addEventListener('input', debounce(function (e) {
+        switch (e.target.id) {
+            case 'name':
+                checkName();
+                break;
+            case 'email':
+                checkEmail();
+                break;
+            case 'message':
+                checkMessage();
+                break;
+        }
+    }));
     
     /**
      * Replaces the form HTML with a thank you message after the form has been submitted.
@@ -29,4 +153,5 @@
                 `;
         document.getElementsByClassName('form')[0].innerHTML = html;
     }
+    
 })();
